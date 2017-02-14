fa93380b8a40a6c0beec5a2a299f2bc4ecd47a13
MarcinMoskala <marcinmoskala@gmail.com>
2017-02-14 07:01:32 +0100

    Rename global gson

--- kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/GsonHelpers.kt
+++ kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/GsonHelpers.kt
@@ -5,17 +5,17 @@ import com.google.gson.GsonBuilder
 import com.google.gson.JsonSyntaxException
 import kotlin.reflect.KClass
 
-var globalGson: Gson = GsonBuilder().create()
+var preferencesGson: Gson = GsonBuilder().create()
 
-internal fun Any?.toJson() = if (this == null) null else globalGson.toJson(this)
+internal fun Any?.toJson() = if (this == null) null else preferencesGson.toJson(this)
 
 @JvmName("toJsonNonNullable")
-internal fun Any.toJson() = globalGson.toJson(this)!!
+internal fun Any.toJson() = preferencesGson.toJson(this)!!
 
 internal inline fun <reified T : Any> String.fromJson() = fromJson(T::class)
 
 internal fun <T : Any> String.fromJson(clazz: KClass<T>) = try {
-    globalGson.fromJson(this, clazz.java)
+    preferencesGson.fromJson(this, clazz.java)
 } catch (e: JsonSyntaxException) {
     throw Error("Error in parsing \"$this\" to ${clazz.java}", e)
 }
