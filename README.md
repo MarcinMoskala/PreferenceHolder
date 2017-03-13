bb0a655400b0720404fb39ef862056d38a881291
MarcinMoskala <marcinmoskala@gmail.com>
2017-03-13 12:09:15 +0100

    Clean-up and readme update

--- README.md
+++ README.md
@@ -3,31 +3,47 @@ Kotlin Android Library, that makes preference usage simple and fun.
 
 [![](https://jitpack.io/v/marcinmoskala/kotlinpreferences.svg)](https://jitpack.io/#marcinmoskala/kotlinpreferences)
 
-With KotlinPreferences, all you can define different preference fields this way:
+With KotlinPreferencesObject, you can define different preference fields this way:
 
 ```kotlin
-var SharedPreferences.canEatPie: Boolean by bindToPreferenceField(true)
+object Pref: PreferenceHolder() {
+    var canEatPie: Boolean by bindToPreferenceField(true)
+}
 ```
 
-And use it like as an extension form any SharedPreference object:
+And use it this way:
 
 ```kotlin
-if(preferences.canEatPie) //...
+if(Pref.canEatPie) //...
 ```
 
-Here are other preference variants: (see [full example](https://github.com/MarcinMoskala/KotlinPreferences/blob/master/kotlinpreferences-lib/src/androidTest/java/com/marcinmoskala/kotlinpreferences/ExampleConfig.kt) and [usage](https://github.com/MarcinMoskala/KotlinPreferences/tree/master/kotlinpreferences-lib/src/androidTest/java/com/marcinmoskala/kotlinpreferences))
+Or with static import:
 
 ```kotlin
-var SharedPreferences.canEatPie: Boolean by bindToPreferenceField(true)
-var SharedPreferences.allPieInTheWorld: Long by bindToPreferenceField(0)
+import com.projectpackage.Pref.canEatPie
+```
 
-var SharedPreferences.isMonsterKiller: Boolean? by bindToPreferenceFieldNullable()
-var SharedPreferences.monstersKilled: Int? by bindToPreferenceFieldNullable()
-var SharedPreferences.experience: Float? by bindToPreferenceFieldNullable()
-var SharedPreferences.className: String? by bindToPreferenceFieldNullable()
+use it this way:
 
-var SharedPreferences.character: Character? by bindToPreferenceFieldNullable()
-var SharedPreferences.savedGame: Game? by bindToPreferenceFieldNullable()
+```kotlin
+if(canEatPie) //...
+```
+
+Here are other preference definition examples: (see [full example](https://github.com/MarcinMoskala/KotlinPreferences/blob/master/kotlinpreferences-lib/src/androidTest/java/com/marcinmoskala/kotlinpreferences/ExampleConfig.kt) and [usage](https://github.com/MarcinMoskala/KotlinPreferences/tree/master/kotlinpreferences-lib/src/androidTest/java/com/marcinmoskala/kotlinpreferences))
+
+```kotlin
+object UserPref: PreferenceHolder() {
+    var canEatPie: Boolean by bindToPreferenceField(true)
+    var allPieInTheWorld: Long by bindToPreferenceField(0)
+
+    var isMonsterKiller: Boolean? by bindToPreferenceFieldNullable()
+    var monstersKilled: Int? by bindToPreferenceFieldNullable()
+    var experience: Float? by bindToPreferenceFieldNullable()
+    var className: String? by bindToPreferenceFieldNullable()
+
+    var character: Character? by bindToPreferenceFieldNullable()
+    var savedGame: Game? by bindToPreferenceFieldNullable()
+}
 ```
 
 ## Install
@@ -36,7 +52,7 @@ To add KotlinPreferences to the project, add in build.gradle file:
 
 ```groovy
 dependencies {
-    compile 'com.github.marcinmoskala:kotlinpreferences:0.03'
+    compile 'com.github.marcinmoskala:kotlinpreferencesobject:0.10'
 }
 ```
 
--- kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/PreferenceHolder.kt
+++ kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/PreferenceHolder.kt
@@ -3,6 +3,8 @@ package com.marcinmoskala.kotlinpreferences
 import android.content.Context
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
@@ -14,88 +16,98 @@ abstract class PreferenceHolder {
     inline fun <reified T : Any> bindToPreferenceFieldNullable(key: String? = null) = PreferenceFieldBinderNullable(T::class, key)
 
     @Suppress("UNCHECKED_CAST")
-    class PreferenceFieldBinder<T : Any>(val clazz: KClass<T>, val default: T?, val key: String? = null) : ReadWriteProperty<PreferenceHolder, T> {
-
-        override operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T = readValue(property, preferences)
-
-        override fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T) {
-            preferences.edit().apply {
-                when (clazz.simpleName) {
-                    "Long" -> putLong(getKey(property), value as Long)
-                    "Int" -> putInt(getKey(property), value as Int)
-                    "String" -> putString(getKey(property), value as String?)
-                    "Boolean" -> putBoolean(getKey(property), value as Boolean)
-                    "Float" -> putFloat(getKey(property), value as Float)
-                    else -> putString(getKey(property), value.toJson())
-                }
-            }.apply()
-        }
-
-        private fun readValue(property: KProperty<*>, thisRef: SharedPreferences): T = when (clazz.simpleName) {
-            "Long" -> thisRef.getLong(getKey(property), default as Long) as T
-            "Int" -> thisRef.getInt(getKey(property), default as Int) as T
-            "String" -> thisRef.getString(getKey(property), default as? String) as T
-            "Boolean" -> thisRef.getBoolean(getKey(property), default as Boolean) as T
-            "Float" -> thisRef.getFloat(getKey(property), default as Float) as T
-            else -> thisRef.getString(getKey(property), default?.toJson()).fromJson(clazz)
-        }
-
-        private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    class PreferenceFieldBinderNullable<T : Any>(val clazz: KClass<T>, val key: String? = null) : ReadWriteProperty<PreferenceHolder, T?> {
-
-        override operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T? = readValue(property, preferences)
-
-        override fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T?) {
-            if (value == null) {
-                removeValue(property, preferences)
-            } else {
-                saveValue(property, preferences, value)
-            }
-        }
-
-        private fun readValue(property: KProperty<*>, thisRef: SharedPreferences): T? {
-            val key = getKey(property)
-            if (!thisRef.contains(key)) return null
-            return when (clazz.simpleName) {
-                "Long" -> thisRef.getLong(key, -1L) as? T
-                "Int" -> thisRef.getInt(key, -1) as? T
-                "String" -> thisRef.getString(key, null) as? T
-                "Boolean" -> thisRef.getBoolean(key, false) as? T
-                "Float" -> thisRef.getFloat(getKey(property), -1.0F) as T
-                else -> thisRef.getString(key, null)?.fromJson(clazz)
-            }
-        }
-
-        private fun removeValue(property: KProperty<*>, thisRef: SharedPreferences) {
-            thisRef.edit().remove(getKey(property)).apply()
-        }
-
-        private fun saveValue(property: KProperty<*>, thisRef: SharedPreferences, value: T?) {
-            thisRef.edit().apply {
-                when (clazz.simpleName) {
-                    "Long" -> putLong(getKey(property), value as Long)
-                    "Int" -> putInt(getKey(property), value as Int)
-                    "String" -> putString(getKey(property), value as String?)
-                    "Boolean" -> putBoolean(getKey(property), value as Boolean)
-                    "Float" -> putFloat(getKey(property), value as Float)
-                    else -> putString(getKey(property), value.toJson())
-                }
-            }.apply()
-        }
-
-        private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
-    }
-
     companion object {
+
         private lateinit var preferences: SharedPreferences
+
+        var preferencesGson: Gson = GsonBuilder().create()
 
         fun setContext(context: Context) {
             preferences = PreferenceManager.getDefaultSharedPreferences(context)
         }
 
         fun clear() = preferences.edit().clear().apply()
+
+        private fun Any?.toJson() = if (this == null) null else preferencesGson.toJson(this)
+
+        private fun <T : Any> String.fromJson(clazz: KClass<T>) = try {
+            preferencesGson.fromJson(this, clazz.java)
+        } catch (e: Throwable) {
+            throw Error("Error in parsing to ${clazz.java}. Possibly lack of gson serializers. The string to parse: \"$this\"", e)
+        }
+
+        class PreferenceFieldBinder<T : Any>(val clazz: KClass<T>, val default: T?, val key: String? = null) : ReadWriteProperty<PreferenceHolder, T> {
+
+            override operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T = readValue(property, preferences)
+
+            override fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T) {
+                preferences.edit().apply {
+                    when (clazz.simpleName) {
+                        "Long" -> putLong(getKey(property), value as Long)
+                        "Int" -> putInt(getKey(property), value as Int)
+                        "String" -> putString(getKey(property), value as String?)
+                        "Boolean" -> putBoolean(getKey(property), value as Boolean)
+                        "Float" -> putFloat(getKey(property), value as Float)
+                        else -> putString(getKey(property), value.toJson())
+                    }
+                }.apply()
+            }
+
+            private fun readValue(property: KProperty<*>, thisRef: SharedPreferences): T = when (clazz.simpleName) {
+                "Long" -> thisRef.getLong(getKey(property), default as Long) as T
+                "Int" -> thisRef.getInt(getKey(property), default as Int) as T
+                "String" -> thisRef.getString(getKey(property), default as? String) as T
+                "Boolean" -> thisRef.getBoolean(getKey(property), default as Boolean) as T
+                "Float" -> thisRef.getFloat(getKey(property), default as Float) as T
+                else -> thisRef.getString(getKey(property), default?.toJson()).fromJson(clazz)
+            }
+
+            private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
+        }
+
+        class PreferenceFieldBinderNullable<T : Any>(val clazz: KClass<T>, val key: String? = null) : ReadWriteProperty<PreferenceHolder, T?> {
+
+            override operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T? = readValue(property, preferences)
+
+            override fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T?) {
+                if (value == null) {
+                    removeValue(property, preferences)
+                } else {
+                    saveValue(property, preferences, value)
+                }
+            }
+
+            private fun readValue(property: KProperty<*>, thisRef: SharedPreferences): T? {
+                val key = getKey(property)
+                if (!thisRef.contains(key)) return null
+                return when (clazz.simpleName) {
+                    "Long" -> thisRef.getLong(key, -1L) as? T
+                    "Int" -> thisRef.getInt(key, -1) as? T
+                    "String" -> thisRef.getString(key, null) as? T
+                    "Boolean" -> thisRef.getBoolean(key, false) as? T
+                    "Float" -> thisRef.getFloat(getKey(property), -1.0F) as T
+                    else -> thisRef.getString(key, null)?.fromJson(clazz)
+                }
+            }
+
+            private fun removeValue(property: KProperty<*>, thisRef: SharedPreferences) {
+                thisRef.edit().remove(getKey(property)).apply()
+            }
+
+            private fun saveValue(property: KProperty<*>, thisRef: SharedPreferences, value: T?) {
+                thisRef.edit().apply {
+                    when (clazz.simpleName) {
+                        "Long" -> putLong(getKey(property), value as Long)
+                        "Int" -> putInt(getKey(property), value as Int)
+                        "String" -> putString(getKey(property), value as String?)
+                        "Boolean" -> putBoolean(getKey(property), value as Boolean)
+                        "Float" -> putFloat(getKey(property), value as Float)
+                        else -> putString(getKey(property), value.toJson())
+                    }
+                }.apply()
+            }
+
+            private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
+        }
     }
 }
