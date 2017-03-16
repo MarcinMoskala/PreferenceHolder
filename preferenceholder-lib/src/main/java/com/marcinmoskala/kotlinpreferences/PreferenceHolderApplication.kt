65c56062a595c232609c88a64c15c786ca70fbbb
MarcinMoskala <marcinmoskala@gmail.com>
2017-03-16 23:07:29 +0100

    Rename to Application extension to PreferenceHolderApplication

--- preferenceholder-lib/src/main/java/com/marcinmoskala/kotlinpreferences/PreferenceHolderApplication.kt
+++ preferenceholder-lib/src/main/java/com/marcinmoskala/kotlinpreferences/PreferenceHolderApplication.kt
@@ -2,7 +2,7 @@ package com.marcinmoskala.kotlinpreferences
 
 import android.app.Application
 
-class KotlinPreferencesObjectApplication : Application() {
+class PreferenceHolderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         PreferenceHolder.setContext(applicationContext)
